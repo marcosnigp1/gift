852c7f10335e8104c3baa11a7110b88336ef9975
Marcos Hernández <marcosdavidho@outlook.es>
2025-12-30 03:21:44 -0500

    1

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ let animation_started = false;
 
 function preload() {
   soundFormats("mp3", "ogg");
-  font = loadFont("font/AmsterdamHandWriting.otf");
+  font = loadFont("font/AmsterdamHandWriting.ttf");
   img = loadImage("images/sun.png");
   heart = loadImage("images/heart.png");
   song = loadSound("song/song.mp3");
